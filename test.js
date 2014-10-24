34cc6d1244edca33f16c8831274ec4849f0ff0fd
jonschlinkert <dev@sellside.com>
2014-10-24 06:31:07 -0400

    ensure that negation patterns work

--- test.js
+++ test.js
@@ -19,6 +19,11 @@ describe('filterKeys', function () {
     filterKeys({foo: 'a', bar: 'b', baz: 'c'}, 'b*').should.eql(['bar', 'baz']);
   });
 
+  it('should exclude keys that match negation patterns:', function () {
+    filterKeys({a: 'a', b: 'b', c: 'c'}, ['*', '!b']).should.eql(['a', 'c']);
+    filterKeys({foo: 'a', bar: 'b', baz: 'c'}, ['*', '!b*']).should.eql(['foo']);
+  });
+
   it('should return an array of keys if no pattern is given:', function () {
     filterKeys({a: 'a', b: 'b', c: 'c'}).should.eql(['a', 'b', 'c']);
   });
