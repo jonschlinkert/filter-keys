783dc735bdb944015ea8a14b89decf961de933a9
jonschlinkert <jon.schlinkert@sellside.com>
2015-01-19 01:11:30 -0500

    expose micromatch options

--- index.js
+++ index.js
@@ -2,17 +2,15 @@
 
 var mm = require('micromatch');
 
-module.exports = function filterKeys(o, patterns) {
+module.exports = function filterKeys(o, patterns, options) {
   if (o == null) {
     throw new Error('filter-keys expects an object');
   }
 
   var keys = Object.keys(o);
-  if (!patterns) return keys;
-
-  if (arguments.length === 1) {
+  if (!patterns || arguments.length === 1) {
     return keys;
   }
 
-  return mm(keys, patterns);
+  return mm(keys, patterns, options);
 };
